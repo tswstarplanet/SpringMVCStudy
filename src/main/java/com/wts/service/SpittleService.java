48fce47ee614b3b4f46753d9c255fe76f9ef8cca
weitaosheng <tswstarplanet@163.com>
2017-02-06 17:33:53 +0800

    增加个人spittle页面

---
+++ src/main/java/com/wts/service/SpittleService.java
@@ -0,0 +1,10 @@
+package com.wts.service;
+
+/**
+ * Created by wtswindows7 on 2017/2/6.
+ */
+public interface SpittleService {
+
+
+
+}
---
+++ src/main/java/com/wts/service/SpittleServiceImpl.java
@@ -0,0 +1,7 @@
+package com.wts.service;
+
+/**
+ * Created by wtswindows7 on 2017/2/6.
+ */
+public class SpittleServiceImpl implements SpittleService {
+}
